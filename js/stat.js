a152cc9b4e62aaed47c0f5748b8e0f969ffc1fc5
July-4n <september4n@gmail.com>
2019-09-17 21:52:21 +0500

    добавлены пробелы

--- js/stat.js
+++ js/stat.js
@@ -6,17 +6,16 @@ var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP_SHADOW = 10;
 var GAP = 20;
-var GAP_FIRST = 40;
 var SPACEBETWEEN = 50;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT_MAX = 150;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 1; i < arr.length; i++) {
@@ -27,7 +26,7 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx, players, times) {
+window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP_SHADOW, CLOUD_Y + GAP_SHADOW, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
@@ -40,10 +39,10 @@ window.renderStatistics = function(ctx, players, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < players.length; i++) {
-  ctx.fillStyle = 'hsl(232, ' + Math.random() * 100 + '%, 87%)';
-  ctx.fillRect(CLOUD_X + GAP * 2 + (SPACEBETWEEN + BAR_WIDTH)* i, (CLOUD_HEIGHT - GAP * 2 - (BAR_HEIGHT_MAX * times[i]) / maxTime),
-  BAR_WIDTH, (BAR_HEIGHT_MAX * times[i]) / maxTime);
-  ctx.fillStyle = '#000';
-  ctx.fillText(players[i], CLOUD_X + GAP * 2 + (SPACEBETWEEN + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
+    ctx.fillStyle = 'hsl(232, ' + Math.random() * 100 + '%, 87%)';
+    ctx.fillRect(CLOUD_X + GAP * 2 + (SPACEBETWEEN + BAR_WIDTH)* i, (CLOUD_HEIGHT - GAP * 2 - (BAR_HEIGHT_MAX * times[i]) / maxTime),
+      BAR_WIDTH, (BAR_HEIGHT_MAX * times[i]) / maxTime);
+    ctx.fillStyle = '#000';
+    ctx.fillText(players[i], CLOUD_X + GAP * 2 + (SPACEBETWEEN + BAR_WIDTH) * i, CLOUD_HEIGHT - GAP);
   }
 };
